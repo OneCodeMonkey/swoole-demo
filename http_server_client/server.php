d199188ebc5315d10408d61069ed124d76b47363
OneCodeMonkey <1460018362@qq.com>
2020-03-08 17:09:05 +0800

    modify

--- http_server_client/server.php
+++ http_server_client/server.php
@@ -2,7 +2,23 @@
 
 $http = new Swoole\Http\Server("0.0.0.0", 9501);
 $http->on('request', function ($request, $response) {
-    var_dump($request);
-    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+    $params = $request['get'];
+    if (!empty($params)) {
+        $fin = fopen(__DIR__ . '/record.txt', "w");
+        fwrite($fin, json_encode($params));
+        fwrite($fin, "\r\n");
+        fclose($fin);
+    }
+    $messageLists = '';
+    $filePath = __DIR__ . '/record.txt';
+    if (file_exists($filePath)) {
+        $fout = fopen($filePath, "r");
+        while (!feof($fout)) {
+            $line = fgets($fout);
+            $messageLists .= $line . "<br/>";
+        }
+    }
+
+    $response->end("<h1>Message list:</h1><br/>" . $messageLists);
 });
 $http->start();
